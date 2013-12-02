99023975374a987602ee514eb480ddb116a870b7
Maxim Kovalov <max.kovalov@gmail.com>
2013-12-02 12:58:17 +0200

    fix minigication for angular

--- Gruntfile.js
+++ Gruntfile.js
@@ -237,6 +237,7 @@ module.exports = function (grunt) {
 
     uglify: {
       dist: {
+        options:{mangle:false},
         src: ['.tmp/public/concat/production.js'],
         dest: '.tmp/public/min/production.js'
       }
