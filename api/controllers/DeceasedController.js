9f120760e53d1ab2433fa5f767a3341ed5bbeb92
Maxim Kovalov <max.kovalov@gmail.com>
2013-12-09 18:01:39 +0200

    API - Deceased and ads update on image upload

--- api/controllers/DeceasedController.js
+++ api/controllers/DeceasedController.js
@@ -19,7 +19,8 @@ var s3 = require("s3")
   , UUIDGenerator = require('node-uuid')
   , _ = require('lodash')
   , fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , async = require('async');
 
 
 module.exports = {
@@ -35,54 +36,70 @@ module.exports = {
 
 
   uploadPhoto:function(req,res,next){
+
     if(!("photo" in req.files)|| Array.isArray(req.files.photo)){
       return res.send(400,{error:"No photo uploaded"});
     }
-    var photo = req.files.photo
-      , s3_client = s3.createClient({
-          key:sails.config.aws.s3_key,
-          secret:sails.config.aws.s3_secret,
-          bucket:sails.config.aws.s3_bucket
-        })
-      , extension = path.extname(photo.name)
-      , uuid = UUIDGenerator.v1()
-      , filename
-      , headers;
+
+    var photo = req.files.photo;
+
+    Deceased.findOne({id:req.param('id')},function deceased_found(err,deceased){
+      if(err || !deceased){
+        clean_fs(photo);
+        return res.send(404,{error:"Could not find deceased"});
+      }
+
+      
+      var s3_client = s3.createClient({
+            key:sails.config.aws.s3_key,
+            secret:sails.config.aws.s3_secret,
+            bucket:sails.config.aws.s3_bucket
+          })
+        , extension = path.extname(photo.name)
+        , uuid = UUIDGenerator.v1()
+        , filename
+        , headers;
 
 
 
-    if(!_.contains(sails.config.file.image_content_types,photo.headers['content-type'])){
-      clean_fs(photo);
-      return res.send(400,{"error":"Invalid content type"});
-    }
-    if(!_.contains(sails.config.file.image_extensions,extension)){
-      clean_fs(photo);
-      return res.send(400,"Invalid extension type");
-    }
+      if(!_.contains(sails.config.file.image_content_types,photo.headers['content-type'])){
+        clean_fs(photo);
+        return res.send(400,{"error":"Invalid content type"});
+      }
+      if(!_.contains(sails.config.file.image_extensions,extension)){
+        clean_fs(photo);
+        return res.send(400,"Invalid extension type");
+      }
 
-    headers = {
-      "Content-Type": photo.headers['content-type']
-    };
+      headers = {
+        "Content-Type": photo.headers['content-type']
+      };
 
-    filename = uuid  + extension;
+      filename = uuid  + extension;
 
-    var uploader = s3_client.upload(photo.path, filename,headers);
+      var uploader = s3_client.upload(photo.path, filename,headers);
 
-    uploader.on("error", function file_upload_error(err){
-      clean_fs(photo);
-      res.send(500,{error:err});
+      uploader.on("error", function file_upload_error(err){
+        clean_fs(photo);
+        res.send(500,{error:err});
+      });
+
+      uploader.on("end",function file_uploaded(){
+        clean_fs(photo);
+        deceased.photo=filename;
+        Ad.update({'deceased.id':deceased.id},{'deceased':deceased},function deceasedAdsFound(err,ads){
+          deceased.save(function save_deceased(err){
+            res.send({photo:filename});
+          })
+        });
+      });
+
+
+      function clean_fs(photo){
+        fs.unlink(photo.path);
+      }
+
     });
-
-    uploader.on("end",function file_uploaded(){
-      clean_fs(photo);
-      res.send({filename:filename});
-    });
-
-
-    function clean_fs(photo){
-      fs.unlink(photo.path);
-    }
   }
-
   
 };
